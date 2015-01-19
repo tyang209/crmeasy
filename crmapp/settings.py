10a7f65ec64207c8730d9697f52a06fd3ad0f65a
tyang209 <tyang209@gmail.com>
2015-01-19 16:38:48 -0500

    first commit

---
+++ crmapp/settings.py
@@ -0,0 +1,102 @@
+"""
+Django settings for crmapp project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#import
+import os
+from django.core.exceptions import ImproperlyConfigured
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def get_env_variable(var_name):
+    """get the environment variable or return exception"""
+    try:
+        return os.environ[var_name]
+    except:
+        error_msg="Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)    
+
+ENV_ROLE=get_env_variable('ENV_ROLE')
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-jduz_@onlo_rj=%1&lgmqie+$mo469p$i(gcu(73n^1=m$d_^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+CRMEASY_DB_PASS= False
+TEMPLATE_DEBUG = DEBUG
+if ENV_ROLE =='development':
+    DEBUG=True
+    TEMPLATE_DEBUG=DEBUG
+    CRMEASY_DB_PASS=get_env_variable('CRMEASY_DB_PASS')
+
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'crmapp.urls'
+
+WSGI_APPLICATION = 'crmapp.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmeasyDB',
+        'USER': 'postgres',
+        'PASSWORD':CRMEASY_DB_PASS,
+        'HOST':'/tmp',
+        'PORT': '5432'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
